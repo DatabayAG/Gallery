8a5bd551c54ec695a9ff2d7db072043b991bb9c5
ay <ay@263b181f-92cb-427f-a063-0eeedfbb7c4c>
2013-08-16 09:03:33 +0000

    fancybox for public galeries

--- templates/tpl.publicgalleries.php
+++ templates/tpl.publicgalleries.php
@@ -1,5 +1,13 @@
-	    <?php foreach($VARS->get('ppictures') as $key => $picture) { ?>
+
+	<?php foreach($VARS->get('ppictures') as $key => $picture) { ?>
 		<div style="margin:0 5px 5px 0;border: solid 1px gray;background-color: white;float:left;padding:5px;">
-		    <a href="<?php echo $VARS->get('contentLink').'&palbum_id='.$_GET['palbum_id'].'&imgout='.$key; ?>&width=1000&height=800" class="lytebox"  onclick="return false;" data-lyte-options="group:album" data-title="<?php echo $picture->get('title');?>"><img src='<?php echo $VARS->get('contentLink').'&palbum_id='.$_GET['palbum_id'].'&imgout='.$key; ?>&width=100&height=100&upscale=1&quad=1' width="100" height="100" /></a><br/>
+		    <a href="<?php echo $VARS->get('contentLink').'&palbum_id='.$_GET['palbum_id'].'&imgout='.$key; ?>&width=1000&height=800" rel="img_group" class="lytebox" data-lyte-options="group:album" data-title="<?php echo $picture->get('title');?>"  title="<?php echo $picture->get('title');?>" onclick="return false;" ><img src='<?php echo $VARS->get('contentLink').'&palbum_id='.$_GET['palbum_id'].'&imgout='.$key; ?>&width=100&height=100&upscale=1&quad=1' title="<?php echo $picture->get('title');?>" width="100" height="100" /></a><br/>
 		</div>
 	    <?php } ?>
+	    
+<script>
+	    $(function() {
+	    		    $('a[rel=img_group]').fancybox({'type' : 'image'});
+	    });
+
+</script>
