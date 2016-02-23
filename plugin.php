2479abe2cc8ae33ca976ac81bddb47565f7d7c8b
ay <ay@263b181f-92cb-427f-a063-0eeedfbb7c4c>
2016-02-23 12:54:44 +0000

    Version erhöht

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 $id = "xgal";
  
 // code version; must be changed for all code changes
-define('xgal_version', '1.2.1');
+define('xgal_version', '1.2.2');
 $version = xgal_version;
  
 // ilias min and max version; must always reflect the versions that should
